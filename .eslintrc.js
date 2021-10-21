0b9a47f3f304b1b16a4c87db1a79d00c3f70dff8
Augusto Teixeira <aught.ceo@gmail.com>
2021-10-20 21:14:59 -0300

    ignored some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,7 @@ module.exports = {
     rules: {
       // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
       // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-unused-vars": "off"
     }
   };
