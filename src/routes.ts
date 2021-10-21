13b004bff4ac35761499ace856f45a895c6fdb91
Augusto Teixeira <aught.ceo@gmail.com>
2021-10-21 19:44:33 -0300

    added get last 3 messages endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
@@ -18,7 +19,11 @@ router.get("/signin/callback", (request, response) => {
   return response.json({ "code": code });
 })
 
+
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
+
+
+router.get("/messages/last3", new GetLast3MessagesController().handle)
 
 
 export { router }
