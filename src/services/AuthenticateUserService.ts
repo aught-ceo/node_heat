7802e1b2f35a7492b29bad8457deba2d64ffc74c
Augusto Teixeira <aught.ceo@gmail.com>
2021-10-20 21:56:59 -0300

    added user information

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,10 +1,21 @@
 import axios from "axios";
 
+interface IAccessTokenResponse {
+  access_token: string
+}
+
+interface IUserResponse {
+  avatar_url: string,
+  login: string,
+  uid: number,
+  name: string
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
     const url = "https://github.com/login/oauth/access_token";
 
-    const response = await axios.post(url, null, {
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -13,10 +24,15 @@ class AuthenticateUserService {
       headers: {
         "Accept": "application/json"
       }
+    });
 
+    const { data: userResponse } = await axios.get<IUserResponse>("https://api.github.com/user", {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`
+      }
     })
 
-    return response.data;
+    return userResponse;
    }
 }
 
