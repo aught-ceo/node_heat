2d78b5888a523152862409cd1dc088aa40733aa9
Augusto Teixeira <aught.ceo@gmail.com>
2021-10-21 19:34:53 -0300

    changed to http server and added socket io

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
 import "dotenv/config";
 import express from "express";
 import { router } from "./routes";
+import http from "http";
+import cors from "cors";
+import { Server, Socket } from "socket.io"
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", socket => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+
+});
+
 app.use(express.json())
 app.use(router);
 
-app.set('port', process.env.PORT || 4000);
-app.set('host', process.env.HOST || 'localhost');
-
-app.listen(app.get('port'), () => console.log(`🚀 Server is running at http://${app.get('host')}:${app.get('port')}`));
+export { serverHttp, io };
---
+++ src/server.ts
@@ -0,0 +1,3 @@
+import { serverHttp } from "./app";
+
+serverHttp.listen(4000, () => console.log(`🚀 Server is running at http://localhost:4000`));
