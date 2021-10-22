017e7230298e931654402d1a83523acb31c77c12
Augusto Teixeira <aught.ceo@gmail.com>
2021-10-21 21:16:01 -0300

    added user profile endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,4 @@
-import { Router } from "express";
-import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
-import { CreateMessageController } from "./controllers/CreateMessageController";
-import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
-import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+import { UserProfileController } from './controllers/UserProfileController';
 
 const router = Router();
 
@@ -23,7 +19,11 @@ router.get("/signin/callback", (request, response) => {
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
 
 
-router.get("/messages/last3", new GetLast3MessagesController().handle)
+router.post(
+  '/profile',
+  ensureAuthenticated,
+  new UserProfileController().handle
+);
 
 
 export { router }
